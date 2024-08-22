645f76bab6db0fb7d6d6d233dd5c3fab53010ff6
MarwenTlili <marwentlili@gmail.com>
2024-08-22 23:09:52 +0100

    perf: improve jest setup

--- backend/jest.setup.ts
+++ backend/jest.setup.ts
@@ -3,21 +3,24 @@ import mongoose from 'mongoose';
 // @mongo: is service name in compose.yaml
 const MONGODB_URI = 'mongodb://test:test@mongo:27017/skeleton_test';
 
-// Connect to the in-memory MongoDB server
+// Connect to the MongoDB server
 beforeAll(async () => {
-  await mongoose.connect(MONGODB_URI);
-}, 30000);
+    try {
+        await mongoose.connect(MONGODB_URI)
+    } catch (error) {
+        console.log('error: ', error)
+    }
+}, 60000)
 
-// Clean up the database before each test
-beforeEach(async () => {
-  const collections = mongoose.connection.collections;
-  for (const key in collections) {
-    const collection = collections[key];
-    await collection.deleteMany({});
-  }
-});
+// Clean up the database collections after each test
+afterEach(async () => {
+    const collections = await mongoose.connection.db.collections()
+    for (let collection of collections) {
+        await collection.deleteMany({})
+    }
+})
 
 // Close the connection after all tests
 afterAll(async () => {
-  await mongoose.disconnect();
-});
+    await mongoose.disconnect()
+})
