91cf964ded18bfed2238330c53ffe7384fe88996
MarwenTlili <marwentlili@gmail.com>
2024-09-16 12:58:14 +0100

    refactor

--- backend/jest.setup.ts
+++ backend/jest.setup.ts
@@ -5,22 +5,22 @@ const MONGODB_URI = 'mongodb://test:test@mongo:27017/skeleton_test';
 
 // Connect to the MongoDB server
 beforeAll(async () => {
-    try {
-        await mongoose.connect(MONGODB_URI)
-    } catch (error) {
-        console.log('error: ', error)
-    }
+  try {
+    await mongoose.connect(MONGODB_URI)
+  } catch (error) {
+    console.log('error: ', error)
+  }
 }, 60000)
 
 // Clean up the database collections after each test
 afterEach(async () => {
-    const collections = await mongoose.connection.db.collections()
-    for (let collection of collections) {
-        await collection.deleteMany({})
-    }
+  const collections = await mongoose.connection.db.collections()
+  for (let collection of collections) {
+    await collection.deleteMany({})
+  }
 })
 
 // Close the connection after all tests
 afterAll(async () => {
-    await mongoose.disconnect()
+  await mongoose.disconnect()
 })
